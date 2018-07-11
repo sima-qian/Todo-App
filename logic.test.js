405107a70438716d9cfdb992eeb12440b3d737bb
Jessie Beech <jessie@jessiebeech.com>
2018-07-11 12:23:16 +0100

    add test division and template

---
+++ logic.test.js
@@ -0,0 +1,25 @@
+var test = require('tape');
+var logic = require('./logic');
+
+test('Example test', function(t) {
+  t.pass();
+  t.end();
+});
+
+
+// Tests for addTodo
+
+
+
+// Tests for deleteTodo
+
+
+
+// Tests for markTodo
+
+
+
+// ( Tests for sortTodos - to be completed later as strech goal)
+
+
+
