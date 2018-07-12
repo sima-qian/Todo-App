6aa4582aa34081329f23f80a8e92501a355b5ddb
sima-qian <29804933+sima-qian@users.noreply.github.com>
2018-07-12 12:59:08 +0100

    Added logic.js for todos sort (unmarked above marked) + testing

--- logic.js
+++ logic.js
@@ -57,11 +57,18 @@ var todoFunctions = {
     // this element will have its done value toggled
     // hint: array.map
   },
-  sortTodos: function(todos, sortFunction) {
+  sortTodos: function(todos) {
     // stretch goal! Do this last
     // should leave the input arguement todos unchanged (you can use cloneArrayOfObjects)
     // sortFunction will have same signature as the sort function in array.sort
     // hint: array.slice, array.sort
+    var done = todos.filter(function(x) {
+      return x.done;
+    });
+    var undone = todos.filter(function(x) {
+      return !x.done;
+    });
+    return undone.concat(done);
   }
 };
 
--- logic.test.js
+++ logic.test.js
@@ -26,61 +26,87 @@ test("addTodo: testing 'eat all the jaffa cakes' returns proper object", functio
   t.end();
 });
 
-test("addTodo: add second entry to existing array of objects", function(t) {
-  var actual = logic.addTodo(
-    [{ id: 0, description: "start diet", done: false }],
-    "eat all the jaffa cakes"
-  );
-  var expected = [
-    { id: 0, description: "start diet", done: false },
-    { id: 3, description: "eat all the jaffa cakes", done: false }
-  ];
-  t.deepEqual(
-    actual,
-    expected,
-    "add 'eat all the jaffa cakes' to 'start diet' todo list"
-  );
-  t.end();
-});
-
-test("addTodo: test object is being cloned/is pure", function(t) {
-  var actual = logic.addTodo(
-    [{ id: 0, description: "start diet", done: false }],
-    "eat all the jaffa cakes"
-  );
-  var expected = [
-    { id: 0, description: "start diet", done: false },
-    { id: 4, description: "eat all the jaffa cakes", done: false }
-  ];
-  t.deepEqual(
-    actual,
-    expected,
-    "add 'eat all the jaffa cakes' to 'start diet' todo list"
-  );
-  t.end();
-});
 // Tests for deleteTodo
 var state = [
-    { id: -3, description: 'first todo', },
-    { id: -2, description: 'second todo' },
-    { id: -1, description: 'third todo' },
-  ];
+  { id: -3, description: "first todo" },
+  { id: -2, description: "second todo" },
+  { id: -1, description: "third todo" }
+];
 
-test('Tests for deleteTodo', function(t){
-    t.deepEqual(logic.deleteTodo([],5), [], "Should return an array");
-    t.deepEqual(logic.deleteTodo([{id: 1, done: false}], 1), [], 'Should delete element with id: 1');
-    t.deepEqual(logic.deleteTodo(state,-2),[{ id: -3, description: 'first todo' },{ id: -1, description: 'third todo' }],"Should delete only one element")
-    t.notStrictEqual(logic.deleteTodo(state,0),state,"Should not return original array")
-    t.end();
-})
+test("Tests for deleteTodo", function(t) {
+  t.deepEqual(logic.deleteTodo([], 5), [], "Should return an array");
+  t.deepEqual(
+    logic.deleteTodo([{ id: 1, done: false }], 1),
+    [],
+    "Should delete element with id: 1"
+  );
+  t.deepEqual(
+    logic.deleteTodo(state, -2),
+    [
+      { id: -3, description: "first todo" },
+      { id: -1, description: "third todo" }
+    ],
+    "Should delete only one element"
+  );
+  t.notStrictEqual(
+    logic.deleteTodo(state, 0),
+    state,
+    "Should not return original array"
+  );
+  t.end();
+});
 
 // Tests for markTodo
 
-test('Tests for markTodo', function(t) {
-    t.deepEqual(logic.markTodo(state, -4) instanceof Array, true, "Should return array");
-    t.notStrictEqual(logic.markTodo(state, -4), state, "Shouldn't return same array as passed in as argument");
-    t.deepEqual(logic.markTodo([{id: -1, done: false}],-1),[{id: -1, done: true}],"Should alter item with id of -1, toggling 'done'");
-    t.deepEqual(logic.markTodo([{id: -1, done: false}, {id: -2, done: false}, {id: -3, done: true}],-1),[{id: -1, done: true}, {id: -2, done: false}, {id: -3, done: true}],"Should only alter item with id of -1, toggling 'done'");
-    t.end();
-})
+test("Tests for markTodo", function(t) {
+  t.deepEqual(
+    logic.markTodo(state, -4) instanceof Array,
+    true,
+    "Should return array"
+  );
+  t.notStrictEqual(
+    logic.markTodo(state, -4),
+    state,
+    "Shouldn't return same array as passed in as argument"
+  );
+  t.deepEqual(
+    logic.markTodo([{ id: -1, done: false }], -1),
+    [{ id: -1, done: true }],
+    "Should alter item with id of -1, toggling 'done'"
+  );
+  t.deepEqual(
+    logic.markTodo(
+      [
+        { id: -1, done: false },
+        { id: -2, done: false },
+        { id: -3, done: true }
+      ],
+      -1
+    ),
+    [{ id: -1, done: true }, { id: -2, done: false }, { id: -3, done: true }],
+    "Should only alter item with id of -1, toggling 'done'"
+  );
+  t.end();
+});
+
 // ( Tests for sortTodos - to be completed later as strech goal)
+
+var stateSort = [
+  { id: -3, description: "first todo", done: true },
+  { id: -2, description: "second todo", done: false },
+  { id: -1, description: "third todo", done: false }
+];
+
+test("Tests for sortTodos", function(t) {
+  t.equal(logic.sortTodos(state) instanceof Array, true, "Should return array");
+  t.deepEqual(
+    logic.sortTodos(stateSort),
+    [
+      { id: -2, description: "second todo", done: false },
+      { id: -1, description: "third todo", done: false },
+      { id: -3, description: "first todo", done: true }
+    ],
+    "Should return sorted array"
+  );
+  t.end();
+});
